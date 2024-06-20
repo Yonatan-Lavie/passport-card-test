b07d80b72806c8ed76d465851dc7e9c87dacdf21
Yonatan Lavie <yonatan.lavie89@gmail.com>
2024-06-20 10:08:13 +0300

    configure the app to use the custom error middleware throw NotFoundError when trying to request incorrect path

--- shopping-cart/src/app.ts
+++ shopping-cart/src/app.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from 'express';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 const port = 3000;
@@ -6,6 +8,12 @@ const port = 3000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
 });
+
+app.use('*', (req: Request, res: Response) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
