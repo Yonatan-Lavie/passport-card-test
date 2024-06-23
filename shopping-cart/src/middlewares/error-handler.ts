cf0f4c78d5031a4d2b37858ab6996be005f68c44
Yonatan Lavie <yonatan.lavie89@gmail.com>
2024-06-23 16:21:39 +0300

    add comments and logs to error handler

--- shopping-cart/src/middlewares/error-handler.ts
+++ shopping-cart/src/middlewares/error-handler.ts
@@ -1,20 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors/custom-error';
 
-
-
+// Middleware function for handling errors
 export const errorHandler = (
-    err: Error,
-    req: Request,
-    res: Response,
-    next: NextFunction
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
+  // Check if the error is an instance of CustomError
+  if (err instanceof CustomError) {
+    console.log(`[Error] ${err.message}`);
+    // If it is, respond with the appropriate status code and serialized errors
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-    if(err instanceof CustomError){
-        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-    }
-
-    return res.status(400).send({
-        errors: [{ message: 'Somthing went wrong' }]
-    });
+  // If the error is not a CustomError, respond with a generic error message
+  return res.status(400).send({
+    errors: [{ message: 'Something went wrong' }],
+  });
 };
