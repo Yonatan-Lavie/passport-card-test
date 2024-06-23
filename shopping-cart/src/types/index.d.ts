a9fa1a5d3466d709260b2f9d0998066db4aaf57f
Yonatan Lavie <yonatan.lavie89@gmail.com>
2024-06-23 16:23:32 +0300

    add comments to query and log event types in index.d.ts file

--- shopping-cart/src/types/index.d.ts
+++ shopping-cart/src/types/index.d.ts
@@ -1,15 +1,17 @@
 // types/index.d.ts
+
+// Type definition for a query event in Prisma
 export type QueryEvent = {
-    timestamp: Date;
-    query: string; // Query sent to the database
-    params: string; // Query parameters
-    duration: number; // Time elapsed (in milliseconds) between client issuing query and database responding
-    target: string;
-  };
-  
-  export type LogEvent = {
-    timestamp: Date;
-    message: string;
-    target: string;
-  };
-  
+  timestamp: Date; // The time when the query was executed
+  query: string; // The query sent to the database
+  params: string; // The parameters used in the query
+  duration: number; // The time elapsed (in milliseconds) between the client issuing the query and the database responding
+  target: string; // The target of the query
+};
+
+// Type definition for a log event in Prisma
+export type LogEvent = {
+  timestamp: Date; // The time when the log event was recorded
+  message: string; // The log message
+  target: string; // The target of the log event
+};
