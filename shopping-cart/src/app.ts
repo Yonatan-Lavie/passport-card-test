097e1da482d77b67ed7484dc244d1df276856aa7
Yonatan Lavie <yonatan.lavie89@gmail.com>
2024-06-23 16:27:51 +0300

    add routers to app add comments

--- shopping-cart/src/app.ts
+++ shopping-cart/src/app.ts
@@ -1,20 +1,34 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import { router as cartRouter } from './routes/cart';
+import { router as productRouter } from './routes/product';
+import { router as userRouter } from './routes/user';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
-});
+// Middleware to parse JSON bodies
+app.use(express.json());
 
+// Route for user-related operations
+app.use('/users', userRouter);
+
+// Route for product-related operations
+app.use('/product', productRouter);
+
+// Route for cart-related operations
+app.use('/cart', cartRouter);
+
+// Catch-all route for handling not found errors
 app.use('*', (req: Request, res: Response) => {
   throw new NotFoundError();
 });
 
+// Error handling middleware
 app.use(errorHandler);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
